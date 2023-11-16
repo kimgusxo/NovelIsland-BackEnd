4236da665ff1d43500eb332d33b3c6ed33bf5205
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2023-11-16 15:27:05 +0900

    error line

--- src/test/java/com/example/novelisland/integration/ElasticSearchNovelTest.java
+++ src/test/java/com/example/novelisland/integration/ElasticSearchNovelTest.java
@@ -1,22 +1,22 @@
-package com.example.novelisland.integration;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-
-@Slf4j
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ElasticSearchNovelTest {
-
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    private String createURLWithPort(String uri) {
-        return "http://localhost:" + port + uri;
-    }
-}
+//package com.example.novelisland.integration;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.test.web.server.LocalServerPort;
+//
+//@Slf4j
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class ElasticSearchNovelTest {
+//
+//    @LocalServerPort
+//    private int port;
+//
+//    @Autowired
+//    private TestRestTemplate restTemplate;
+//
+//    private String createURLWithPort(String uri) {
+//        return "http://localhost:" + port + uri;
+//    }
+//}
