c39d1c5637826a6f94bf07f1c845bfb2c303305b
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2024-03-06 03:53:34 +0900

    elasticsearch controller unit test complete

--- src/test/java/com/example/novelisland/NovelIslandApplicationTests.java
+++ src/test/java/com/example/novelisland/NovelIslandApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.novelisland;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NovelIslandApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.novelisland;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NovelIslandApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
