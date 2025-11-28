00e4a181914099b14d246ad2d58c9f8b10012dd1
김현태 <gusxoas9906@gmail.com>
2025-11-29 04:41:39 +0900

    [Refactor] 메세지 포맷 간소화 및 중복 configuration 수정

--- src/main/java/com/example/novelisland/format/Message.java
+++ src/main/java/com/example/novelisland/format/Message.java
@@ -1,32 +1,57 @@
 package com.example.novelisland.format;
 
+import com.example.novelisland.description.ErrorCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Builder
 @Getter
-public class Message {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Message<T> {
 
+    @ApiModelProperty(example = "성공여부")
+    private final boolean success;
     @ApiModelProperty(example = "상태코드")
-    private int statusCode;
+    private final int statusCode;
     @ApiModelProperty(example = "메세지")
-    private String message;
+    private final String message;
     @ApiModelProperty(example = "응답데이터")
-    private Object data;
+    private final T data;
 
-    public static Message of(String message, int statusCode, Object data){
-        return Message.builder()
-                .message(message)
-                .statusCode(statusCode)
+    public static <T> Message<T> success(T data) {
+        return Message.<T>builder()
+                .success(true)
+                .statusCode(HttpStatus.OK.value())
+                .message("요청이 성공했습니다.")
                 .data(data)
                 .build();
     }
 
-    public static Message of(String message, int statusCode){
-        return Message.builder()
+    public static <T> Message<T> success(String message, T data) {
+        return Message.<T>builder()
+                .success(true)
+                .statusCode(HttpStatus.OK.value())
                 .message(message)
-                .statusCode(statusCode)
+                .data(data)
+                .build();
+    }
+
+    public static Message<Void> success(String message) {
+        return Message.<Void>builder()
+                .success(true)
+                .statusCode(HttpStatus.OK.value())
+                .message(message)
+                .build();
+    }
+
+    public static Message<Void> failure(ErrorCode errorCode) {
+        return Message.<Void>builder()
+                .success(false)
+                .statusCode(errorCode.getHttpStatus().value())
+                .message(errorCode.getDetail())
                 .build();
     }
 }
