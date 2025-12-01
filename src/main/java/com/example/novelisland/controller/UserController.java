43c8d622592a4da612bbf28e233ab414e785154c
김현태 <gusxoas9906@gmail.com>
2025-12-01 21:40:48 +0900

    [Refactor] LoginController 보일러 플레이트 코드 수정 및 로그 리팩토링

--- src/main/java/com/example/novelisland/controller/UserController.java
+++ src/main/java/com/example/novelisland/controller/UserController.java
@@ -4,9 +4,8 @@ import com.example.novelisland.dto.UserDTO;
 import com.example.novelisland.format.Message;
 import com.example.novelisland.service.UserService;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,28 +13,34 @@ import javax.validation.Valid;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @PutMapping
     @ApiOperation(value = "유저정보 수정", notes = "유저의 정보를 수정한다.")
-    public ResponseEntity<Message> updateUser(@RequestBody @Valid UserDTO userDTO) {
-        log.info("updateUser: {}", userDTO);
-        return new ResponseEntity<>(Message.of("회원정보 수정 완료", HttpStatus.OK.value(), userService.updateUser(userDTO)), HttpStatus.OK);
+    public ResponseEntity<Message<?>> updateUser(@RequestBody @Valid UserDTO userDTO) {
+        log.info("[PUT /users] 회원정보 수정 요청 - userDTO={}", userDTO);
+
+        return ResponseEntity.ok(
+                Message.success(
+                        "회원정보 수정 완료",
+                        userService.updateUser(userDTO)
+                )
+        );
     }
 
     @DeleteMapping("/{userIndex}")
     @ApiOperation(value = "유저정보 삭제", notes = "유저의 정보를 삭제한다.")
-    public ResponseEntity<Message> deleteUser(@PathVariable("userIndex") Long userIndex) {
-        log.info("deleteUser: {}", userIndex);
+    public ResponseEntity<Message<?>> deleteUser(@PathVariable("userIndex") Long userIndex) {
+        log.info("[DELETE /users/{}] 회원정보 삭제 요청 - userIndex={}", userIndex, userIndex);
+
         userService.deleteUser(userIndex);
-        return new ResponseEntity<>(Message.of("회원정보 삭제 완료", HttpStatus.OK.value()), HttpStatus.OK);
+
+        return ResponseEntity.ok(
+                Message.success("회원정보 삭제 완료")
+        );
     }
 }
