5b5904bfa353ee7e2e69a05fc6e4fa309ea6e417
김현태 <gusxoas9906@gmail.com>
2025-12-01 21:40:43 +0900

    [Refactor] LoginController 보일러 플레이트 코드 수정 및 로그 리팩토링

--- src/main/java/com/example/novelisland/controller/LoginController.java
+++ src/main/java/com/example/novelisland/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.example.novelisland.dto.LoginDTO;
 import com.example.novelisland.format.Message;
 import com.example.novelisland.service.LoginService;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,33 +15,49 @@ import javax.validation.Valid;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/login")
 public class LoginController {
-    private final LoginService loginService;
 
-    @Autowired
-    public LoginController(LoginService loginService) {
-        this.loginService = loginService;
-    }
+    private final LoginService loginService;
 
     @GetMapping("/duplicateCheck")
     @ApiOperation(value = "아이디 중복확인", notes = "가입된 아이디가 있는지 확인한다.")
-    public ResponseEntity<Message> duplicateCheck(@RequestParam("userId") String userId) {
-        log.info("duplicateCheck: {}", userId);
-        return new ResponseEntity<>(Message.of("아이디 중복확인 성공", HttpStatus.OK.value(), loginService.duplicateCheck(userId)), HttpStatus.OK);
+    public ResponseEntity<Message<?>> duplicateCheck(@RequestParam("userId") String userId) {
+        log.info("[GET /login/duplicateCheck] 아이디 중복확인 요청 - userId={}", userId);
+
+        return ResponseEntity.ok(
+                Message.success(
+                        "아이디 중복확인 성공",
+                        loginService.duplicateCheck(userId)
+                )
+        );
     }
 
     @PostMapping("/signUp")
     @ApiOperation(value = "회원가입", notes = "유저가 회원가입을 한다.")
-    public ResponseEntity<Message> signUp(@RequestBody @Valid LoginDTO loginDTO) {
-        log.info("signUp: {}", loginDTO);
-        return new ResponseEntity<>(Message.of("회원가입 성공", HttpStatus.OK.value(), loginService.signUp(loginDTO)), HttpStatus.OK);
+    public ResponseEntity<Message<?>> signUp(@RequestBody @Valid LoginDTO loginDTO) {
+        log.info("[POST /login/signUp] 회원가입 요청 - loginDTO={}", loginDTO);
+
+        return ResponseEntity.ok(
+                Message.success(
+                        "회원가입 성공",
+                        loginService.signUp(loginDTO)
+                )
+        );
     }
+
 
     @PostMapping("/signIn")
     @ApiOperation(value = "로그인", notes = "유저가 로그인을 한다.")
-    public ResponseEntity<Message> signIn(@RequestBody @Valid LoginDTO loginDTO) {
-        log.info("signIn: {}", loginDTO);
-        return new ResponseEntity<>(Message.of("로그인 성공", HttpStatus.OK.value(), loginService.signIn(loginDTO)), HttpStatus.OK);
+    public ResponseEntity<Message<?>> signIn(@RequestBody @Valid LoginDTO loginDTO) {
+        log.info("[POST /login/signIn] 로그인 요청 - loginDTO={}", loginDTO);
+
+        return ResponseEntity.ok(
+                Message.success(
+                        "로그인 성공",
+                        loginService.signIn(loginDTO)
+                )
+        );
     }
 }
