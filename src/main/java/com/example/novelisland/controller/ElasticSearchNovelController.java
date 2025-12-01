8bbd9760bd805d6434fec2969a54b7bf7253f266
김현태 <gusxoas9906@gmail.com>
2025-12-01 21:24:48 +0900

    [Refactor] ElasticSearchNovelController 보일러 플레이트 코드 수정 및 로그 리팩토링

--- src/main/java/com/example/novelisland/controller/ElasticSearchNovelController.java
+++ src/main/java/com/example/novelisland/controller/ElasticSearchNovelController.java
@@ -2,34 +2,39 @@ package com.example.novelisland.controller;
 
 import com.example.novelisland.format.Message;
 import com.example.novelisland.service.ElasticSearchNovelService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/elastics")
 public class ElasticSearchNovelController {
 
     private final ElasticSearchNovelService elasticSearchNovelService;
 
-    @Autowired
-    public ElasticSearchNovelController(ElasticSearchNovelService elasticSearchNovelService) {
-        this.elasticSearchNovelService = elasticSearchNovelService;
-    }
-
     @GetMapping
-    public ResponseEntity<Message> getNovelsByNovelExplanation(@RequestParam("novelExplanation") String novelExplanation) {
-        log.info("getNovelByNovelExplanation: {}", novelExplanation);
-        return new ResponseEntity<>(Message.of("소설 검색 성공", HttpStatus.OK.value(),
-                elasticSearchNovelService.getElasticNovelsByNovelExplanation(novelExplanation)), HttpStatus.OK);
+    public ResponseEntity<Message<?>> getNovelsByNovelExplanation(@RequestParam("novelExplanation") String novelExplanation) {
+        log.info("[GET /elastics] 소설 설명 기반 검색 요청 - novelExplanation={}", novelExplanation);
+
+        return ResponseEntity.ok(
+                Message.success(
+                        "소설 검색 성공",
+                        elasticSearchNovelService.getElasticNovelsByNovelExplanation(novelExplanation)
+                )
+        );
     }
 
     @PostMapping("/all")
-    public String insertAll() {
+    public ResponseEntity<Message<?>> insertAll() {
+        log.info("[POST /elastics/all] 전체 소설 인덱싱 요청");
+
         elasticSearchNovelService.insertAllNovels();
-        return "데이터 인덱싱 완료";
+
+        return ResponseEntity.ok(
+                Message.success("데이터 인덱싱 완료")
+        );
     }
 }
