0c58331f5eee259eb425cdc9560a3314ad6c0294
김현태 <gusxoas9906@gmail.com>
2025-12-04 03:24:04 +0900

    [Refactor] globalExceptionHandler 헬퍼메소드 작성 및 로그 리팩토링

--- src/main/java/com/example/novelisland/exception/ControllerExceptionHandler.java
+++ src/main/java/com/example/novelisland/exception/ControllerExceptionHandler.java
@@ -11,69 +11,87 @@ import com.example.novelisland.exception.login.InvalidPasswordException;
 import com.example.novelisland.exception.novel.NotExistNovelException;
 import com.example.novelisland.exception.tag.NotExistTagException;
 import com.example.novelisland.exception.user.NotExistUserException;
-import com.example.novelisland.format.ErrorMessage;
-import org.springframework.http.HttpStatus;
+import com.example.novelisland.format.Message;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@Slf4j
+@RestControllerAdvice
 public class ControllerExceptionHandler {
 
+    private ResponseEntity<Message<?>> buildErrorResponse(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(Message.failure(errorCode));
+    }
+
     @ExceptionHandler(ConcurrentlySignUpException.class)
-    public ResponseEntity<ErrorMessage> handleConcurrentlySignUpException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.CONCURRENTLY_SIGNUP_TOKEN), HttpStatus.CONFLICT);
+    public ResponseEntity<Message<?>> handleConcurrentlySignUpException(ConcurrentlySignUpException ex) {
+        log.warn("[ConcurrentlySignUpException] 동시 회원가입 예외 발생", ex);
+        return buildErrorResponse(ErrorCode.CONCURRENTLY_SIGNUP_TOKEN);
     }
 
     @ExceptionHandler(DuplicateIdException.class)
-    public ResponseEntity<ErrorMessage> handleDuplicatedIdException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.DUPLICATE_ID_TOKEN), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Message<?>> handleDuplicatedIdException(DuplicateIdException ex) {
+        log.warn("[DuplicateIdException] 아이디 중복 예외 발생", ex);
+        return buildErrorResponse(ErrorCode.DUPLICATE_ID_TOKEN);
     }
 
     @ExceptionHandler(InvalidIdException.class)
-    public ResponseEntity<ErrorMessage> handleInvalidIdException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.INVALID_ID_TOKEN), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Message<?>> handleInvalidIdException(InvalidIdException ex) {
+        log.warn("[InvalidIdException] 잘못된 아이디 예외 발생", ex);
+        return buildErrorResponse(ErrorCode.INVALID_ID_TOKEN);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity<ErrorMessage> handleInvalidPasswordException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.INVALID_PASSWORD_TOKEN), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Message<?>> handleInvalidPasswordException(InvalidPasswordException ex) {
+        log.warn("[InvalidPasswordException] 잘못된 비밀번호 예외 발생", ex);
+        return buildErrorResponse(ErrorCode.INVALID_PASSWORD_TOKEN);
     }
 
     @ExceptionHandler(NotExistUserException.class)
-    public ResponseEntity<ErrorMessage> handleNotExistUserException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.NOT_EXIST_USER_TOKEN), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Message<?>> handleNotExistUserException(NotExistUserException ex) {
+        log.warn("[NotExistUserException] 유저 미존재 예외 발생", ex);
+        return buildErrorResponse(ErrorCode.NOT_EXIST_USER_TOKEN);
     }
 
     @ExceptionHandler(NotExistNovelException.class)
-    public ResponseEntity<ErrorMessage> handleNotExistNovelException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.NOT_EXIST_NOVEL_TOKEN), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Message<?>> handleNotExistNovelException(NotExistNovelException ex) {
+        log.warn("[NotExistNovelException] 소설 미존재 예외 발생", ex);
+        return buildErrorResponse(ErrorCode.NOT_EXIST_NOVEL_TOKEN);
     }
 
     @ExceptionHandler(NotExistAuthorException.class)
-    public ResponseEntity<ErrorMessage> handleNotExistAuthorException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.NOT_EXIST_AUTHOR_TOKEN), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Message<?>> handleNotExistAuthorException(NotExistAuthorException ex) {
+        log.warn("[NotExistAuthorException] 작가 미존재 예외 발생", ex);
+        return buildErrorResponse(ErrorCode.NOT_EXIST_AUTHOR_TOKEN);
     }
 
     @ExceptionHandler(NotExistBookMarkException.class)
-    public ResponseEntity<ErrorMessage> handleNotExistBookMarkException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.NOT_EXIST_BOOKMARK_TOKEN), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Message<?>> handleNotExistBookMarkException(NotExistBookMarkException ex) {
+        log.warn("[NotExistBookMarkException] 북마크 미존재 예외 발생", ex);
+        return buildErrorResponse(ErrorCode.NOT_EXIST_BOOKMARK_TOKEN);
     }
 
     @ExceptionHandler(NotExistTagException.class)
-    public ResponseEntity<ErrorMessage> handleNotExistTagException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.NOT_EXIST_TAG_TOKEN), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Message<?>> handleNotExistTagException(NotExistTagException ex) {
+        log.warn("[NotExistTagException] 태그 미존재 예외 발생", ex);
+        return buildErrorResponse(ErrorCode.NOT_EXIST_TAG_TOKEN);
     }
 
     @ExceptionHandler(AlreadyExistBookMarkException.class)
-    public ResponseEntity<ErrorMessage> handleAlreadyExistBookMarkException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.AlREADY_EXIST_BOOKMARK_TOKEN), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Message<?>> handleAlreadyExistBookMarkException(AlreadyExistBookMarkException ex) {
+        log.warn("[AlreadyExistBookMarkException] 이미 존재하는 북마크 예외 발생", ex);
+        return buildErrorResponse(ErrorCode.AlREADY_EXIST_BOOKMARK_TOKEN);
     }
 
-    // 유효성 실패했을때의 예외처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException() {
-        return new ResponseEntity<>(ErrorMessage.of(ErrorCode.NOT_VALIDATION_TOKEN), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Message<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.warn("[MethodArgumentNotValidException] 요청 값 유효성 검증 실패", ex);
+        return buildErrorResponse(ErrorCode.NOT_VALIDATION_TOKEN);
     }
+
 }
